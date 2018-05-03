51df63d92f50e2cb3c70f514ed23f8a60cb53385
Leo Enge <leoe@kth.se>
2018-05-03 14:32:08 +0200

    made pyscript more interactive

--- PythonExternal/set_obstacle.py
+++ PythonExternal/set_obstacle.py
@@ -1,21 +1,46 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from matplotlib.collections import PatchCollection
 import sys
+import os
+
+f = open(os.devnull, 'w')
+sys.stderr = f
 
 grid_size = int(sys.argv[1])
+obstacle_str = sys.argv[2]
+eligible_obstacles_str = sys.argv[3]
+
+#grid_size = 6
+#obstacle_str = '1,1;3,4'
+#eligible_obstacles_str = '2,2;1,4'
 
 fig = plt.figure()
 ax = fig.add_subplot(111)
 ax.axis([0, grid_size, 0, grid_size])
 
+plt.hold(True)
+
 for i in range(1, grid_size):
     ax.axhline(i, 0, grid_size, c = 'r')
     ax.axvline(i, 0, grid_size, c = 'r')
 
-xy_list = plt.ginput(-1)
-datastr = ""
-for xypair in xy_list:
-    datastr = datastr + str(grid_size - 1 - int(xypair[1])) + ',' + str(int(xypair[0])) + ';'
+obstacles = [] #using (row, col) not (x, y)
+if(obstacle_str != ''):
+    for rowcol_str in obstacle_str.split(';'):
+        obstacles.append(Rectangle((int(rowcol_str.split(',')[1]), grid_size - 1 - int(rowcol_str.split(',')[0])), 1, 1))
+ax.add_collection(PatchCollection(obstacles, facecolor='k'))
+
+eligible_obstacles = [] #using (row, col) not (x, y)
+if(eligible_obstacles_str != ''):
+    for rowcol_str in eligible_obstacles_str.split(';'):
+        eligible_obstacles.append(Rectangle((int(rowcol_str.split(',')[1]), grid_size - 1 - int(rowcol_str.split(',')[0])), 1, 1))
+ax.add_collection(PatchCollection(eligible_obstacles, facecolor='b'))
+
+xy = plt.ginput(1)
+print(xy)
+datastr = str(grid_size - 1 - int(xy[0][1])) + ',' + str(int(xy[0][0]))
 print(datastr)
 sys.exit(0)
 
